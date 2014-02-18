65c16e289a56d058ae052893a8b8c7a86ff32821
Tobi Lehman <tobi.lehman@gmail.com>
2014-02-17 19:56:59 -0800

    add AsyncTask to open a stream socket and write some data to it

--- src/com/definidev/netcat/MainActivity.java
+++ src/com/definidev/netcat/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -13,12 +14,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		final EditText ipEdit = (EditText)findViewById(R.id.ip_address);
+		final EditText portEdit = (EditText)findViewById(R.id.port);
+		final EditText bodyEdit = (EditText)findViewById(R.id.body);
+		
 		final Button sendButton = (Button)findViewById(R.id.send_button);
 		
 		sendButton.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
+				String ip = ipEdit.getText().toString();
+				String port = portEdit.getText().toString();
+				String body = bodyEdit.getText().toString();
+
+				new SendBodyTask().execute(ip, port, body);
 			}
 		});
 	}
---
+++ src/com/definidev/netcat/SendBodyTask.java
@@ -0,0 +1,39 @@
+package com.definidev.netcat;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.lang.Integer;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class SendBodyTask extends AsyncTask<String, Void, Void> {
+
+	@Override
+	protected Void doInBackground(String... args) {
+		try {
+			String ip = args[0];
+			int port = Integer.parseInt(args[1]);
+			String body = args[2];
+
+			InetSocketAddress addr = new InetSocketAddress(ip, port);
+			Socket socket = new Socket();
+			socket.connect(addr);
+			OutputStream stream = socket.getOutputStream();
+			stream.write(body.getBytes());
+			socket.close();
+
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			Log.e("SocketExceptionTag", e.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Log.e("IOExceptionTag", e.toString());
+		}
+		return null;
+	}
+
+}
